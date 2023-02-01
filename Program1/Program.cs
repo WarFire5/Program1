057ae11c958390ab3630b38e0a4a42e23bdb6eb6
Татьяна <@WIN-E362SIULNK8>
2023-02-02 02:08:37 +0300

    Добавьте файлы проекта.

---
+++ Program1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientFirstName;
+
+            Console.WriteLine("Как Вас зовут?");
+            clientFirstName = Console.ReadLine();
+
+            string clientAppeal = "Привет," + " " + "*" + clientFirstName + "*" + "!!!";
+            Console.WriteLine(clientAppeal);
+
+            Console.ReadLine();
+        }
+    }
+}
